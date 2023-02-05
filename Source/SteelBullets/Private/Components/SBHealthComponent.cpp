ea5c58060ccbae7c92eea25561e6df6fdc326816
apokrif6 <j.berenchan@gmail.com>
2023-02-05 19:09:17 +0100

    [COMBAT] Add autoheal

--- Source/SteelBullets/Private/Components/SBHealthComponent.cpp
+++ Source/SteelBullets/Private/Components/SBHealthComponent.cpp
@@ -12,7 +12,7 @@ void USBHealthComponent::BeginPlay()
 {
 	Super::BeginPlay();
 
-	Health = MaxHealth;
+	IncreaseHealth(MaxHealth);
 
 	AActor* ComponentOwner = GetOwner();
 
@@ -27,13 +27,29 @@ void USBHealthComponent::OnTakeAnyDamage(AActor* DamagedActor, float Damage, con
 {
 	if (IsDead()) return;
 
-	Health = FMath::Clamp(Health - Damage, 0.0f, MaxHealth);
-	OnHealthChanged.Broadcast(Health);
+	DecreaseHealth(Damage);
 
 	if (IsDead())
 	{
 		OnDeath.Broadcast();
 	}
+	else if (CanAutoHeal)
+	{
+		GetWorld()->GetTimerManager().SetTimer(AutoHealTimer, this, &USBHealthComponent::AutoHeal,
+		                                       AutoHealFrequency, true, AutoHealDelay);
+	}
+}
+
+void USBHealthComponent::IncreaseHealth(const float HealthToIncrease)
+{
+	Health = FMath::Clamp(Health + HealthToIncrease, 0.0f, MaxHealth);
+	OnHealthChanged.Broadcast(Health);
+}
+
+void USBHealthComponent::DecreaseHealth(const float HealthToDecrease)
+{
+	Health = FMath::Clamp(Health - HealthToDecrease, 0.0f, MaxHealth);
+	OnHealthChanged.Broadcast(Health);
 }
 
 float USBHealthComponent::GetHealth() const
@@ -44,4 +60,14 @@ float USBHealthComponent::GetHealth() const
 bool USBHealthComponent::IsDead() const
 {
 	return Health <= 0.0f;
+}
+
+void USBHealthComponent::AutoHeal()
+{
+	IncreaseHealth(HPToAutoHeal);
+
+	if (Health >= MaxHealth)
+	{
+		GetWorld()->GetTimerManager().ClearTimer(AutoHealTimer);
+	}
 }
--- Source/SteelBullets/Public/Components/SBHealthComponent.h
+++ Source/SteelBullets/Public/Components/SBHealthComponent.h
@@ -30,12 +30,32 @@ protected:
 	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Health", Meta = (ClampMin = "50.0", ClampMax = "200.0"))
 	float MaxHealth = 100.0f;
 
+	UPROPERTY(EditDefaultsOnly, Category = "Health")
+	bool CanAutoHeal = true;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Health", Meta = (ClampMin = "0.0", ClampMax = "10.0", EditCondition = "CanAutoHeal"))
+	float AutoHealDelay = 3.0f;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Health", Meta = (ClampMin = "0.1", ClampMax = "10.0", EditCondition = "CanAutoHeal"))
+	float AutoHealFrequency = 0.5f;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Health", Meta = (ClampMin = "1.0", ClampMax = "10.0", EditCondition = "CanAutoHeal"))
+	float HPToAutoHeal = 1.0f;
+
 	UFUNCTION()
 	void OnTakeAnyDamage(AActor* DamagedActor, float Damage, const UDamageType* DamageType,
 	                     AController* InstigatedBy, AActor* DamageCauser);
+
+	void AutoHeal();
 
 	virtual void BeginPlay() override;
 
 private:
 	float Health = 0.0f;
+
+	FTimerHandle AutoHealTimer;
+
+	void IncreaseHealth(float HealthToIncrease);
+
+	void DecreaseHealth(float HealthToDecrease);
 };
